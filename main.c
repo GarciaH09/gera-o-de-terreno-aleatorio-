94a1202c970bf36ae34650de78ca88b9f3109b6f
GarciaH09 <142926643+GarciaH09@users.noreply.github.com>
2026-02-01 13:01:46 -0300

    Add creature struct and spawning functions

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 // Online C compiler to run C program online
 #include <stdio.h>
+#include <stdlib.h>  // Aqui moram o rand() e o srand()
+#include <time.h>    // Aqui mora o time(NULL)
 #define LINHAS 8
 #define COLUNAS 9
+typedef struct {
+    int vida ;
+    int dano ;
+ //  char letra[2];
+}criatura ;
 void iniciar_mapa(char mapa[LINHAS][COLUNAS]){
     int i , j ; 
     for( i = 0 ; i<LINHAS ;i++){
@@ -27,9 +34,42 @@ void desenhar_mapa(char mapa[LINHAS][COLUNAS]){
             printf("\n");
         }
     };
+void s_criatura(criatura *lobo){
+    lobo->vida == 2 ;
+    lobo->dano == 3 ;
+    //lobo->letra == "L";
+}
+void spaw(int x , int y,char mapa[LINHAS][COLUNAS]){
+    if (x > 0 && x < LINHAS - 1 && y > 0 && y < COLUNAS - 1) {
+        mapa[x][y] = 'L'; 
+    }
+    else{
+        printf("Nenhum animal foi encontrado \n");
+    };
+    printf("%d %d\n",x,y);
+}
+void mais_criaturas(char mapa[LINHAS][COLUNAS],int x,int y){
+    for(int i = 0 ; i< 5; i++){
+        x +=1 ; y +=1 ;
+        if (x > 0 && x < LINHAS - 1 && y > 0 && y < COLUNAS - 1) {
+            mapa[x][y] = 'L'; 
+         }
+    }
+    desenhar_mapa(mapa);
+}
 int main() {
+// SPAW 
+ criatura lobo ;
+ s_criatura(&lobo);
+srand(time(NULL));
+int x = rand()%LINHAS ;
+int y = rand()%COLUNAS ;
+///MAPA 
 char mapa[LINHAS][COLUNAS];
 iniciar_mapa(mapa);
-desenhar_mapa(mapa);
+spaw(x,y,mapa);
+//desenhar_mapa(mapa);
+mais_criaturas(mapa,x,y);
+
     return 0;
 }
